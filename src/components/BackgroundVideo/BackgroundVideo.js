0357219a1eec8c6f3aced3eb2f1094ed559854aa
James Karnes <karnes.james@gmail.com>
2022-10-14 18:04:56 -0400

    updated video format

--- src/components/BackgroundVideo/BackgroundVideo.js
+++ src/components/BackgroundVideo/BackgroundVideo.js
@@ -1,5 +1,5 @@
 import React from "react"
-import video from "../../video/Coral.mp4"
+import video from "../../video/coral.webm"
 
 const BackroundVideo = () => {
   return (
@@ -13,7 +13,7 @@ const BackroundVideo = () => {
           playsInline
           src={video}
         >
-          <source src={video} type="video/mp4" />
+          <source src={video} type="video/webm" />
           Your device does not support playing 'video/mp4' videos
         </video>
       </div>
