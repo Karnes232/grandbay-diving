268c4641aed5234b1eac5cc84f72b09bc19a366d
James Karnes <karnes.james@gmail.com>
2022-10-29 18:36:52 -0400

    changed theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         short_name: `Grand Bay`,
         start_url: `/`,
         background_color: `#f7f0eb`,
-        theme_color: `#0c16d9`,
+        theme_color: `#1f2937`,
         display: `standalone`,
       },
     },
