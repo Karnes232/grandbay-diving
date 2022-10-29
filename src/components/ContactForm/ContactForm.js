471607777a933aa11a611563dad88ed0a3639784
James Karnes <karnes.james@gmail.com>
2022-10-29 18:57:15 -0400

    testing contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -16,13 +16,13 @@ const ContactForm = () => {
           </p>
         )}
       </div>
-      <form
-        name="contact"
+      <form    
+        name="contact" 
         method="POST"
         action="/contact/?success=true"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        id="contact"
+        id='contact'
         className="w-64 md:w-full max-w-md flex flex-col justify-center items-center mx-auto my-5"
       >
         <input type="hidden" name="form-name" value="contact" />
