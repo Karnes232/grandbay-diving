27e5c7b6fcbf4e244a589a4864688817e0addc78
James Karnes <karnes.james@gmail.com>
2022-10-29 12:26:03 -0400

    prefetch googletagmanager

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex flex-col justify-between overflow-x-hidden">
       <Helment>
         <link rel="dns-prefetch" href="https://www.google-analytics.com" />
+        <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
       </Helment>
       <Header />
       {children}
