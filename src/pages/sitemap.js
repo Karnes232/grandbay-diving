c77826ff6fe30db441500f3c0192abdfd0d4b501
James Karnes <karnes.james@gmail.com>
2022-10-29 09:45:30 -0400

    added sitemap

---
+++ src/pages/sitemap.js
@@ -0,0 +1,269 @@
+import React from "react"
+
+const topStyle = {
+  backgroundColor: "#b1d1e8",
+  fontSize: "16px",
+  paddingBottom: "40px",
+}
+
+const navStyle = {
+  fontSize: "24px",
+  margin: "0px 30px 0px",
+  borderBottomLeftRadius: "6px",
+  borderBottomRightRadius: "6px",
+  backgroundColor: "#f3f3f3",
+  color: "#666",
+  boxShadow:
+    "0 10px 20px -12px rgba(0, 0, 0, 0.42), 0 3px 20px 0px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
+  padding: "10px 0",
+  textAlign: "center",
+  zIndex: 1,
+}
+
+const h3Style = {
+  margin: "auto",
+  padding: "10px",
+  maxWidth: "600px",
+  color: "#666",
+}
+
+const h3SpanStyle = {
+  float: "right",
+}
+
+const h3aStyle = {
+  fontWeight: "normal",
+  display: "block",
+}
+
+const contStyle = {
+  position: "relative",
+  borderRadius: "6px",
+  boxShadow:
+    "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);",
+  background: "#f3f3f3",
+  margin: "-20px 30px 0px 30px",
+  padding: "20px",
+}
+
+const footerStyle = {
+  padding: "10px",
+  textAlign: "center",
+}
+
+const lHeadStyle = {
+  background: "#ddd",
+  padding: "10px",
+  margin: "10px 0px",
+}
+
+const lCountStyle = {
+  padding: "0px 10px",
+}
+
+const lPageStyle = {
+  borderBottom: "#ddd 1px solid",
+  padding: "5px",
+}
+
+const lastPageStyle = {
+  padding: "5px",
+}
+
+const ulStyle = {
+  margin: "0px",
+  padding: "0px",
+  listStyle: "none",
+}
+
+
+const sitemap = () => {
+  return (
+    <>
+      <div style={topStyle} id="top">
+        <nav style={navStyle}>grandbay-2.netlify.app HTML Site Map</nav>
+        <h3 style={h3Style}>
+          <span style={h3SpanStyle}>
+            Last updated: 2022, October 29
+            <br />
+            Total pages: 13
+          </span>
+          <a style={h3aStyle} href="https://grandbay-2.netlify.app">
+            grandbay-2.netlify.app Homepage
+          </a>
+        </h3>
+      </div>
+      <div style={contStyle} id="cont">
+        <ul style={ulStyle} class="level-0">
+          <li style={lHeadStyle} class="lhead">
+            /{" "}
+            <span style={lCountStyle} class="lcount">
+              1 pages
+            </span>
+          </li>
+
+          <li style={lastPageStyle} class="lpage last-page">
+            <a
+              href="https://grandbay-2.netlify.app/"
+              title="https://grandbay-2.netlify.app/"
+            >
+              https://grandbay-2.netlify.app/
+            </a>
+          </li>
+          <li>
+            <ul style={ulStyle} class="level-1">
+              <li style={lHeadStyle} class="lhead">
+                contact/{" "}
+                <span style={lCountStyle} class="lcount">
+                  1 pages
+                </span>
+              </li>
+
+              <li style={lastPageStyle} class="lpage last-page">
+                <a
+                  href="https://grandbay-2.netlify.app/contact/"
+                  title="https://grandbay-2.netlify.app/contact/"
+                >
+                  https://grandbay-2.netlify.app/contact/
+                </a>
+              </li>
+            </ul>
+            <ul style={ulStyle} class="level-1">
+              <li style={lHeadStyle} class="lhead">
+                courses/{" "}
+                <span style={lCountStyle} class="lcount">
+                  5 pages
+                </span>
+              </li>
+
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/courses/"
+                  title="https://grandbay-2.netlify.app/courses/"
+                >
+                  https://grandbay-2.netlify.app/courses/
+                </a>
+              </li>
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/courses/discover"
+                  title="https://grandbay-2.netlify.app/courses/discover"
+                >
+                  https://grandbay-2.netlify.app/courses/discover
+                </a>
+              </li>
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/courses/scubadiver"
+                  title="https://grandbay-2.netlify.app/courses/scubadiver"
+                >
+                  https://grandbay-2.netlify.app/courses/scubadiver
+                </a>
+              </li>
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/courses/openwater"
+                  title="https://grandbay-2.netlify.app/courses/openwater"
+                >
+                  https://grandbay-2.netlify.app/courses/openwater
+                </a>
+              </li>
+              <li style={lastPageStyle} class="lpage last-page">
+                <a
+                  href="https://grandbay-2.netlify.app/courses/advanced"
+                  title="https://grandbay-2.netlify.app/courses/advanced"
+                >
+                  https://grandbay-2.netlify.app/courses/advanced
+                </a>
+              </li>
+            </ul>
+            <ul style={ulStyle} class="level-1">
+              <li style={lHeadStyle} class="lhead">
+                sites/{" "}
+                <span style={lCountStyle} class="lcount">
+                  1 pages
+                </span>
+              </li>
+
+              <li style={lastPageStyle} class="lpage last-page">
+                <a
+                  href="https://grandbay-2.netlify.app/sites/"
+                  title="https://grandbay-2.netlify.app/sites/"
+                >
+                  https://grandbay-2.netlify.app/sites/
+                </a>
+              </li>
+            </ul>
+            <ul style={ulStyle} class="level-1">
+              <li style={lHeadStyle} class="lhead">
+                species/{" "}
+                <span style={lCountStyle} class="lcount">
+                  1 pages
+                </span>
+              </li>
+
+              <li style={lastPageStyle} class="lpage last-page">
+                <a
+                  href="https://grandbay-2.netlify.app/species/"
+                  title="https://grandbay-2.netlify.app/species/"
+                >
+                  https://grandbay-2.netlify.app/species/
+                </a>
+              </li>
+            </ul>
+            <ul style={ulStyle} class="level-1">
+              <li style={lHeadStyle} class="lhead">
+                trips/{" "}
+                <span style={lCountStyle} class="lcount">
+                  4 pages
+                </span>
+              </li>
+
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/trips/"
+                  title="https://grandbay-2.netlify.app/trips/"
+                >
+                  https://grandbay-2.netlify.app/trips/
+                </a>
+              </li>
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/trips/catalina"
+                  title="https://grandbay-2.netlify.app/trips/catalina"
+                >
+                  https://grandbay-2.netlify.app/trips/catalina
+                </a>
+              </li>
+              <li style={lPageStyle} class="lpage">
+                <a
+                  href="https://grandbay-2.netlify.app/trips/saona"
+                  title="https://grandbay-2.netlify.app/trips/saona"
+                >
+                  https://grandbay-2.netlify.app/trips/saona
+                </a>
+              </li>
+              <li style={lastPageStyle} class="lpage last-page">
+                <a
+                  href="https://grandbay-2.netlify.app/trips/bayahibe"
+                  title="https://grandbay-2.netlify.app/trips/bayahibe"
+                >
+                  https://grandbay-2.netlify.app/trips/bayahibe
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div id="footer" style={footerStyle}>
+        Page created with{" "}
+        <a target="_blank" rel="noreferrer" href="https://www.xml-sitemaps.com">
+          Google XML sitemap and html sitemaps generator
+        </a>{" "}
+        | Copyright &copy; 2005-2022 XML-Sitemaps.com
+      </div>
+    </>
+  )
+}
+
+export default sitemap
