5cdd2c564b3c220c1e38be0c546269bf0cbf0751
James Karnes <karnes.james@gmail.com>
2023-04-07 15:42:30 -0400

    formating

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -4,7 +4,6 @@ import SEO from "../components/seo"
 import Layout from "../components/layout"
 import HeroComponent from "../components/HeroComponent/HeroComponent"
 
-
 const thankyou = () => {
   return (
     <Layout>
@@ -18,12 +17,13 @@ const thankyou = () => {
       <HeroComponent page="courses" />
       <div className="mt-[45vh] md:mt-[40vh] lg:mt-[60vh] xl:mt-[65vh]"></div>
       <div className="relative z-0 mb-6 w-full group">
-      <p className="text-black text-center text-xl my-5">
-            Thanks for your message!{" "}
+        <p className="text-black text-center text-xl my-5">
+          Thanks for your message!{" "}
         </p>
         <p className="text-black text-center text-xl my-3">
-            We will get back to you right away{" "}
-        </p></div>
+          We will get back to you right away{" "}
+        </p>
+      </div>
     </Layout>
   )
 }
