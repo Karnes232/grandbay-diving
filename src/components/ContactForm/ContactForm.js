a2cdd7b4fa3cca784d03c5d7b1e3ddfcd55c1466
James Karnes <karnes.james@gmail.com>
2023-04-07 13:25:00 -0400

    adding google recaptcha

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react"
+import Recaptcha from 'react-google-recaptcha'
+const RECAPTCHA_KEY = '6LcvbmolAAAAAENbwLdLXsEC302XbuxY22MaLuHF'
 
 const ContactForm = () => {
   const [success, setSuccess] = useState(false)
+  const recaptchaRef = React.createRef()
+  const [buttonDisabled, setButtonDisabled] = useState(true)
   useEffect(() => {
     if (window.location.search.includes("success=true")) {
       setSuccess(true)
@@ -59,7 +63,7 @@ const ContactForm = () => {
             Email address
           </label>
         </div>
-        <div class="relative z-0 mb-6 w-full group">
+        <div className="relative z-0 mb-6 w-full group">
           <label
             for="message"
             className="block mb-2 text-sm font-medium text-gray-900"
@@ -74,9 +78,17 @@ const ContactForm = () => {
             placeholder="Leave a comment..."
           ></textarea>
         </div>
-        <div data-netlify-recaptcha="true"></div>
+        <Recaptcha
+        className="mb-6"
+        ref={recaptchaRef}
+        sitekey={RECAPTCHA_KEY}
+        size="normal"
+        id="recaptcha-google"
+        onChange={() => setButtonDisabled(false)}
+      />
         <button
           type="submit"
+          disabled={buttonDisabled}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
