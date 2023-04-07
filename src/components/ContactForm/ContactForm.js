539232cb96fb4dc4595b17bfc6a745ac9846d2c5
James Karnes <karnes.james@gmail.com>
2023-04-07 13:32:43 -0400

    adding google recaptcha

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react"
-import Recaptcha from 'react-google-recaptcha'
-const RECAPTCHA_KEY = '6LcvbmolAAAAAENbwLdLXsEC302XbuxY22MaLuHF'
 
 const ContactForm = () => {
   const [success, setSuccess] = useState(false)
-  const recaptchaRef = React.createRef()
-  const [buttonDisabled, setButtonDisabled] = useState(true)
   useEffect(() => {
     if (window.location.search.includes("success=true")) {
       setSuccess(true)
@@ -26,7 +22,6 @@ const ContactForm = () => {
         action="/contact/?success=true"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
-        data-netlify-recaptcha="true"
         id="contact"
         className="w-64 md:w-full max-w-md flex flex-col justify-center items-center mx-auto my-5"
       >
@@ -63,7 +58,7 @@ const ContactForm = () => {
             Email address
           </label>
         </div>
-        <div className="relative z-0 mb-6 w-full group">
+        <div class="relative z-0 mb-6 w-full group">
           <label
             for="message"
             className="block mb-2 text-sm font-medium text-gray-900"
@@ -78,17 +73,9 @@ const ContactForm = () => {
             placeholder="Leave a comment..."
           ></textarea>
         </div>
-        <Recaptcha
-        className="mb-6"
-        ref={recaptchaRef}
-        sitekey={RECAPTCHA_KEY}
-        size="normal"
-        id="recaptcha-google"
-        onChange={() => setButtonDisabled(false)}
-      />
+
         <button
           type="submit"
-          disabled={buttonDisabled}
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
