be93779858e4d97a65ea621dbf645e784b605765
James Karnes <karnes.james@gmail.com>
2024-04-07 17:29:31 -0400

    added google tag for ads

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ["G-6MJLJ90SSM", "AW-482060503"],
+        trackingIds: ["AW-482060503", "G-6MJLJ90SSM"],
         pluginConfig: {
           head: false,
           respectDNT: true,
