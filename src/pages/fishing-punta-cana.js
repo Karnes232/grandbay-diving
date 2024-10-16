83d943952b4ec0ff289cd55e94057e26f195e790
James Karnes <karnes.james@gmail.com>
2024-10-16 17:46:58 -0400

    updated more information about private charters and prices

--- src/pages/fishing-punta-cana.js
+++ src/pages/fishing-punta-cana.js
@@ -40,7 +40,7 @@ const Fishing = () => {
           <div className="lg:flex lg:flex-col lg:justify-start lg:mt-5 xl:min-h-full xl:justify-center xl:mt-0">
             <TextComponent
               title="Deep Sea Fishing"
-              paragraph="You'll cruise out into the open ocean in search of perfect fishing locations, cast your fishing rods over the edge of the boat and wait in beautiful surroundings for a fish to take the bait with other like-minded guests. You can go on either a morning or afternoon expedition and if you're not into fishing, you can just enjoy the boat trip."
+              paragraph="Set sail on a thrilling open ocean adventure as you seek out prime fishing spots, cast your lines, and relax in the stunning surroundings, waiting for that exciting moment when a fish bites. Whether you choose a morning or afternoon expedition, you'll enjoy the camaraderie of fellow guests. Not into fishing? No problem—you can simply unwind and take in the beautiful views on a scenic boat trip instead."
             />
           </div>
           <div className="lg:w-[45rem] xl:mx-10 lg:min-h-full lg:flex lg:flex-col md:justify-center">
@@ -49,9 +49,15 @@ const Fishing = () => {
           <div className="lg:flex lg:flex-col lg:justify-start lg:mt-5 xl:min-h-full xl:justify-center xl:mt-0">
             <TextComponent
               // title={course.title2}
-              paragraph="You'll have four hours of tracking and fishing. Of course, it won't be all work and no play – there'll be multiple fishing rods so that every guest can take it in turns to reel the prize in. While you're waiting patiently, you'll be able to help yourself from the self-service cooler on deck. But make sure you keep a close eye on the rods for signs of a bite as you never know when luck might strike. And if fishings not your thing, take the time to catch some rays on deck and enjoy a soft drink."
+              paragraph="Get ready for four hours of fishing excitement! With several rods at the ready, each guest will have a turn to reel in the catch of the day. While waiting, feel free to grab a cold drink from the self-serve cooler on deck, but don’t forget to keep an eye on the lines—you never know when a bite might happen! And if fishing isn’t your style, kick back, soak up the sun, and enjoy the ocean breeze with a refreshing beverage in hand."
             />
           </div>
+        </div>
+        <div className="flex flex-col lg:flex-row lg:mx-auto max-w-6xl mb-10">
+        <TextComponent
+              // title={course.title2}
+              paragraph="For a more private and personalized experience, we offer exclusive charters. Choose a 4-hour private charter for $700, a 5-hour for $800, a 6-hour charter for $1,085, or extend your adventure with an 8-hour trip for $1,365. These private charters give you the flexibility to tailor the day to your preferences, ensuring a memorable and relaxing experience on the water."
+            />
         </div>
         <Suspense fallback={<div>Loading...</div>}>
           <BackgroundCarousel course={fishing} className="discover-carousel" />
