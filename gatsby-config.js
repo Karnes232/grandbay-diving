84918196df47e1ebf481fe5cb2536e2eba866251
James Karnes <karnes.james@gmail.com>
2024-12-15 16:40:43 -0400

    remove sw

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,8 +15,8 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
     "gatsby-plugin-robots-txt",
-    "gatsby-plugin-offline",
-    // "gatsby-plugin-remove-serviceworker",
+    // "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
